c2ec1b4c2ba673fb4412cd9943b55b91f2d9113e
Indermohan Singh <indermohansinghk7@gmail.com>
2025-03-21 21:29:16 +0000

    fix: prettier errors

--- @ragakosh/taal/src/lib/index.ts
+++ @ragakosh/taal/src/lib/index.ts
@@ -2,20 +2,16 @@ import { taals } from '../taals';
 import type { Taal } from '../types';
 
 function getTaals(): readonly Taal[] {
-    return taals;
+  return taals;
 }
 
 function getTaalByName(name: string): Taal | undefined {
-    const filteredTaal = taals.filter(taal => taal.name === name);
-    return filteredTaal.length ? filteredTaal[0] : undefined;
+  const filteredTaal = taals.filter((taal) => taal.name === name);
+  return filteredTaal.length ? filteredTaal[0] : undefined;
 }
 
 function getTaalByBeats(beats: number): readonly Taal[] {
-    return taals.filter(taal => taal.beats === beats);
+  return taals.filter((taal) => taal.beats === beats);
 }
 
-export {
-    getTaals,
-    getTaalByName,
-    getTaalByBeats
-};
+export { getTaals, getTaalByName, getTaalByBeats };
--- @ragakosh/taal/src/types/index.ts
+++ @ragakosh/taal/src/types/index.ts
@@ -1,11 +1,11 @@
 type Vibhag = {
-    readonly beat: number;  // Beat number in the cycle
-    readonly value: string; // Clap (X), Wave (0), or other markers (2, 3, etc.)
+  readonly beat: number; // Beat number in the cycle
+  readonly value: string; // Clap (X), Wave (0), or other markers (2, 3, etc.)
 };
 
 export type Taal = {
-    readonly name: string;         // Name of the taal (e.g., Teentaal, Dadra)
-    readonly beats: number;        // Total beats in the cycle
-    readonly vibhag: readonly Vibhag[]; // Ensuring vibhag array is read-only
-    readonly notes?: readonly string[]; // Ensuring notes array is read-only
+  readonly name: string; // Name of the taal (e.g., Teentaal, Dadra)
+  readonly beats: number; // Total beats in the cycle
+  readonly vibhag: readonly Vibhag[]; // Ensuring vibhag array is read-only
+  readonly notes?: readonly string[]; // Ensuring notes array is read-only
 };
